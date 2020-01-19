fb856087fb824fcf9c9d9717787b64bcac17a89a
Alex Otoo <alexotoous@outlook.com>
2020-01-19 10:55:13 +0000

    done

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -17,9 +17,14 @@ class ElementAttribute {
 }
 
 class Components {
-  constructor(renderHookId) {
+  constructor(renderHookId, delayedRender = true) {
     this.hookId = renderHookId;
+    if (delayedRender) {
+      this.render();
+    }
   }
+  render() {}
+
   createRootElement(tag, cssClasses, attributes) {
     const rootElement = document.createElement(tag);
     if (cssClasses) {
@@ -72,8 +77,9 @@ class ShoppingCart extends Components {
 
 class ProductItem extends Components {
   constructor(product, renderHookId) {
-    super(renderHookId);
+    super(renderHookId, false);
     this.product = product;
+    this.render();
   }
   addToCart() {
     console.log("adding product to cart....");
@@ -102,48 +108,59 @@ class ProductItem extends Components {
 }
 
 class ProductsList extends Components {
+  products = [];
   constructor(renderHookId) {
     super(renderHookId);
+    this.fetchProducts();
   }
-  products = [
-    new Product(
-      "Flex Cars",
-      "assets/img/car6.JPG",
-      569.08,
-      "new with 2020 software upgrades"
-    ),
-    new Product(
-      "Moon Car",
-      "assets/img/car1.JPG",
-      708.99,
-      "fast and affordable for the new generation"
-    ),
-    new Product(
-      "universal Car",
-      "assets/img/car4.JPG",
-      204.97,
-      "has a multidimentional compactibility with inbuilt meter"
-    )
-  ];
+  fetchProducts() {
+    this.products = [
+      new Product(
+        "Flex Cars",
+        "assets/img/car6.JPG",
+        569.08,
+        "new with 2020 software upgrades"
+      ),
+      new Product(
+        "Moon Car",
+        "assets/img/car1.JPG",
+        708.99,
+        "fast and affordable for the new generation"
+      ),
+      new Product(
+        "universal Car",
+        "assets/img/car4.JPG",
+        204.97,
+        "has a multidimentional compactibility with inbuilt meter"
+      )
+    ];
+    this.renderProdducts();
+  }
+  renderProdducts() {
+    for (const prod of this.products) {
+      new ProductItem(prod, "prod-list");
+    }
+  }
 
   render() {
     this.createRootElement("ul", "product-list", [
       new ElementAttribute("id", "prod-list")
     ]);
 
-    for (const prod of this.products) {
-      const productItem = new ProductItem(prod, "prod-list");
-      const prodEl = productItem.render();
+    if (this.products && this.Products.length > 0) {
+      this.renderProdducts();
     }
   }
 }
 
 class Shop {
+  constructor() {
+    this.render();
+  }
   render() {
     this.cart = new ShoppingCart("app"); //make it a property of shop to be able to use static on it
-    this.cart.render();
-    const productsList = new ProductsList("app");
-    productsList.render();
+
+    new ProductsList("app");
   }
 }
 
@@ -152,7 +169,6 @@ class App {
   static cart;
   static init() {
     const shop = new Shop();
-    shop.render();
     this.cart = shop.cart;
   }
   static addProductToCart(product) {
